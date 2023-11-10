b9f47af64431e628b5caea09fd60e04aa8d40d05
johrad <jpdragic@proton.me>
2023-11-10 11:31:05 +0100

    changed name to battery_percentage

--- pi-side/collectData.py
+++ pi-side/collectData.py
@@ -8,8 +8,8 @@ def collectData():
 
     ## add the collection stuff code and stuff stuff here.
     attackBoolean = r.randint(0,1)
-    bms_voltage = r.randint(0,12)
+    battery_percentage = r.randint(0,12)
     compressorState = r.randint(0,1)
 
-    data = {'attackBoolean': True, 'bms_voltage': 11.2, 'compressorState': False}
+    data = {'attackBoolean': True, 'battery_percentage': battery_percentage, 'compressorState': False}
     return data
--- pi-side/send_data.py
+++ pi-side/send_data.py
@@ -3,7 +3,7 @@ from config import serverAddress
 
 
 def send(data):  
-    data = {'attackBoolean': True, 'bms_voltage': 11.2, 'compressorState': False}
+    data = {'attackBoolean': True, 'battery_percentage': 11.2, 'compressorState': False}
 
     serverAddress = "http://199.247.7.163:8055"
     localhost = 'http://localhost:8044'
