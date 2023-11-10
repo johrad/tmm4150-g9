accf016aa981d9fb7cb5ed7d7ddc1f4335ff0006
johrad <jpdragic@proton.me>
2023-11-10 11:22:06 +0100

    fixed pi side for post request

--- pi-side/collectData.py
+++ pi-side/collectData.py
@@ -7,9 +7,9 @@ import requests
 def collectData():
 
     ## add the collection stuff code and stuff stuff here.
-    attackBoolean = True
+    attackBoolean = r.randint(0,1)
     bms_voltage = r.randint(0,12)
-    compressorState = False
+    compressorState = r.randint(0,1)
 
     data = {'attackBoolean': True, 'bms_voltage': 11.2, 'compressorState': False}
     return data
