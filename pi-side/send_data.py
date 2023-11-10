f0b798e5bd72e973c7b05811b36f9b0d146165c0
johrad <jpdragic@proton.me>
2023-11-10 11:32:18 +0100

    removed bug

--- pi-side/send_data.py
+++ pi-side/send_data.py
@@ -4,8 +4,7 @@ from config import serverAddress
 
 def send(data):  
     data = {'attackBoolean': True, 'battery_percentage': 11.2, 'compressorState': False}
-
-    serverAddress = "http://199.247.7.163:8055"
+    
     localhost = 'http://localhost:8044'
     # Send the POST request object and receive the response
     response = requests.post(url=f'{serverAddress}/upload', json=data)
