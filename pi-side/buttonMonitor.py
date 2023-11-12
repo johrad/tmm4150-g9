de394796612ced35d94dab80588c3e0949d794d8
johrad <jpdragic@proton.me>
2023-11-12 20:12:58 +0100

    bugfix

--- pi-side/buttonMonitor.py
+++ pi-side/buttonMonitor.py
@@ -5,9 +5,6 @@ from signal import pause
 attackButton = Button("GPIO4", pull_up=False)
 compressorButton = Button("GPIO18", pull_up=False)
 
-def a():
-    print("cock")
-
 attackButton.when_pressed = lambda: send(data={'attackBoolean': 1, 'compressorBoolean': 0})
 compressorButton.when_pressed = lambda: send(data={'attackBoolean': 0, 'compressorBoolean': 1})
 
