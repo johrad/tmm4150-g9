002a77ce2748eb79fbdca901e3aac82eaff0915d
johrad <jpdragic@proton.me>
2023-11-13 19:24:16 +0100

    fixed bug

--- piStart.sh
+++ piStart.sh
@@ -2,7 +2,7 @@
 echo "starting program"
 cd "~/tmm4150-g9"
 while true; do
-    python3 main.py
+    python3 pi-side/main.py
     if [[ "$?" != "0" ]]; then
         echo "main.py crashed with exit code $?. Respawning.." >&2
         sleep 1
